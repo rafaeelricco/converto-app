e8bdbd5564618d3bfbfb46aa06779b332086297c
rafaeelricco <rafaelricco10@gmail.com>
2024-09-02 21:19:10 -0300

    styles: added transition to progress component

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -2,16 +2,20 @@ const ProgressCircle: React.FC<{ progress: number }> = ({ progress }) => {
    const stroke_dash_offset = 100 - progress
 
    return (
-      <div className="relative size-4">
-         <svg className="size-full -rotate-90" viewBox="0 0 36 36" xmlns="http://www.w3.org/2000/svg">
+      <div className="relative size-4 transition-all duration-300">
+         <svg
+            className="size-full -rotate-90 transition-all duration-300"
+            viewBox="0 0 36 36"
+            xmlns="http://www.w3.org/2000/svg"
+         >
             {/* <!-- Background Circle --> */}
             <circle
                cx="18"
                cy="18"
                r="16"
                fill="none"
-               className="stroke-current text-[#D9D9D9]"
-               stroke-width="4"
+               className="stroke-current text-[#D9D9D9] transition-all duration-300"
+               strokeWidth="4"
             ></circle>
             {/*  <!-- Progress Circle --> */}
             <circle
@@ -19,11 +23,11 @@ const ProgressCircle: React.FC<{ progress: number }> = ({ progress }) => {
                cy="18"
                r="16"
                fill="none"
-               className="stroke-current text-[#2D2D2D]"
-               stroke-width="4"
-               stroke-dasharray="100"
-               stroke-dashoffset={stroke_dash_offset}
-               stroke-linecap="round"
+               className="stroke-current text-[#2D2D2D] transition-all duration-300"
+               strokeWidth="4"
+               strokeDasharray="100"
+               strokeDashoffset={stroke_dash_offset}
+               strokeLinecap="round"
             ></circle>
          </svg>
       </div>
